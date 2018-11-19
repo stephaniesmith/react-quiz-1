a78dedd00e3f6e272a35509f7ec4c27e925bc108
Sarah Flynn <sarah.flynn@icloud.com>
2018-11-19 15:57:14 -0800

    App h1 rendering to UI

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from 'react';
+
+export default class App extends Component {
+    state = {
+        
+    };
+
+    render() {
+
+        return (
+            <Fragment>
+                <h1>React Quiz</h1>
+            </Fragment>
+        );
+    }
+}
