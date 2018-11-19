a40f70076adeb02ccb8c5d43350c6569ce90fe7f
Claire Flanagan <claireflanagan@gmail.com>
2018-11-19 15:57:47 -0800

    header showing up on screen

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, { Component, Fragment } from 'react';
+// import domToImage from 'dom-to-image';
+
+export default class App extends Component {
+    state={
+      text: ''
+    };
+
+    // onInputChange = ({ target }) => {
+    //   this.setState({ [target.name]: target.value, text: '' });
+    // };
+
+    render() {
+      // const text = this.state;
+      return (
+        <Fragment>
+          <h1>React Quiz</h1>
+          
+        </Fragment>
+      );
+    }
+}
