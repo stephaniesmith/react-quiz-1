2df70a8076d0ace86065afdf83c74d5db481a61f
Stephanie Smith <stephanie.lauren.design@gmail.com>
2018-11-20 09:23:46 -0800

    removing files to practice

--- src/components/Shape.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Shape({ text, color, backgroundColor }) {
-  return (
-    <div style={{ color, backgroundColor, width: '100px', height: '100px' }}>
-      {text}
-    </div>
-  );
-}
