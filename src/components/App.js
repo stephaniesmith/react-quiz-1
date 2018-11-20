0136eaa00d5fbed9b70430892f101d51934f140d
Claire Flanagan <claireflanagan@gmail.com>
2018-11-19 16:40:15 -0800

    shape.js in progress

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,11 @@
 import React, { Component, Fragment } from 'react';
 import TextSelector from './TextSelector';
 import ColorSelector from './ColorSelector';
+import Shape from './Shape';
 
 export default class App extends Component {
     state={
-      text: ''
     };
-
-    // onInputChange = ({ target }) => {
-    //   this.setState({ [target.name]: target.value, text: '' });
-    // };
 
     render() {
       return (
@@ -17,6 +13,7 @@ export default class App extends Component {
           <h1>React Quiz</h1>
           <TextSelector />
           <ColorSelector />
+          <Shape />
         </Fragment>
       );
     }
--- src/components/Shape.js
+++ src/components/Shape.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+// import { color, background } from './ColorSelector';
+// import { text } from './TextSelector';
 
 export default class Shape extends Component {
+  constructor(props) {
+    super(props);
+  }
+
     state = {
+      text: '',
+      color: '',
+      background: ''
     };
 
     render() {
+      const { text, color, background } = this.state;
       return (
-        <div></div>
+        <div style={{ color, background }}>{text}</div>
       ); 
     }
 }
