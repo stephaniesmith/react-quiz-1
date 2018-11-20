a6ae08345a7ac18cb814ed11bae6ba34e5c5d56a
Sarah Flynn <sarah.flynn@icloud.com>
2018-11-19 16:09:39 -0800

    text displays to UI in app

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,25 @@ import React, { Component, Fragment } from 'react';
 
 export default class App extends Component {
     state = {
-        
+        textInput: ''
+    };
+
+    onChange = ({ target }) => {
+        console.log('target', target.name, target.value);
+        this.setState({ [target.name]: target.value });
     };
 
     render() {
-
+        const { textInput } = this.state;
         return (
             <Fragment>
                 <h1>React Quiz</h1>
+                <form>
+                    <label> Text: 
+                        <input name="textInput" value={textInput} onChange={this.onChange} />
+                    </label>
+                </form>
+                {textInput && <p>{textInput}</p> }
             </Fragment>
         );
     }
