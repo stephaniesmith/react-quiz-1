2a5cbd1da5cd5e0a2ed657de78ccddf39999c58f
Claire Flanagan <claireflanagan@gmail.com>
2018-11-19 16:15:08 -0800

    input working

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import TextSelector from './TextSelector';
 // import domToImage from 'dom-to-image';
 
 export default class App extends Component {
@@ -11,11 +12,10 @@ export default class App extends Component {
     // };
 
     render() {
-      // const text = this.state;
       return (
         <Fragment>
           <h1>React Quiz</h1>
-          
+          <TextSelector />
         </Fragment>
       );
     }
